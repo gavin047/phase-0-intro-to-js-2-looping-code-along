58734b8fff5f426318c8dc264e4025035e53de8e
gavin <gavinmichori@gmail.com>
2024-07-24 11:07:40 +0300

    .git/config

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+function countDown(num) {
+    for (let i = num; i >= 0; i--) {
+        console.log(i);
+    }
+}
+countDown(10);
+
+
+function writeCards(names, event) {
+    const messages = [];
+    for (let i = 0; i < names.length; i++) {
+        const message = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+        messages.push(message);
+    }
+    return messages;
+}const messages = writeCards(["Charlie", "Samip", "Ali"], "birthday");
+console.log(messages);
